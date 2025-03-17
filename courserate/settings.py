329faa14ecda3dae54075cad0afc08573cb36635
jwenham <jrwenham22@gmail.com>
2025-03-17 11:29:38 -0700

    Fix STATIC_URL and Whitenoise settings

--- courserate/settings.py
+++ courserate/settings.py
@@ -138,9 +138,10 @@ LOGOUT_REDIRECT_URL = 'home'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_URL = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Required for collectstatic
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
